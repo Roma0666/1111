92fcda550e4dcf7ef37b6e9fd5624ccc3346877a
Roman <134275917+Roma0666@users.noreply.github.com>
2024-12-30 17:39:17 +0100

    Add files via upload

--- blog/find-post.php
+++ blog/find-post.php
@@ -1,9 +1,9 @@
 <?php
 
-include ('boostrap.php');
+include('boostrap.php');
 echo '<link rel="stylesheet" href="style.css">';
 $searchTerm = isset($_GET['search']) ? mysqli_real_escape_string($conn, $_GET['search']) : '';
-
+var_dump($searchTerm);
 $sql = "
     SELECT 
         posts.id, 
@@ -12,12 +12,13 @@ $sql = "
         posts.date, 
         users.username AS author_name, 
         categories.categories_name AS category_name,
-        GROUP_CONCAT(tags.tags_name SEPARATOR ', ') AS tag_names
+        (SELECT GROUP_CONCAT(tags.tags_name ORDER BY tags.tags_name ASC SEPARATOR ', ') 
+         FROM post_tags 
+         JOIN tags ON post_tags.tag_id = tags.id 
+         WHERE post_tags.post_id = posts.id) AS tag_names
     FROM posts
     JOIN users ON posts.id_author = users.id
     JOIN categories ON posts.categories_id = categories.id
-    LEFT JOIN post_tags ON posts.id = post_tags.post_id
-    LEFT JOIN tags ON post_tags.tag_id = tags.id
     WHERE 1
 ";
 
@@ -28,11 +29,17 @@ if ($searchTerm) {
         posts.title LIKE '%$searchTerm%' OR 
         posts.description LIKE '%$searchTerm%' OR 
         categories.categories_name LIKE '%$searchTerm%' OR 
-        tags.tags_name LIKE '%$searchTerm%'
+        EXISTS (
+            SELECT 1
+            FROM post_tags
+            JOIN tags ON post_tags.tag_id = tags.id
+            WHERE post_tags.post_id = posts.id
+            AND tags.tags_name LIKE '%$searchTerm%'
+        )
     )";
 }
 
-$sql .= " GROUP BY posts.id ORDER BY posts.date DESC";
+$sql .= " ORDER BY posts.date DESC";
 
 $result = $conn->query($sql);
 
@@ -44,7 +51,7 @@ if ($result->num_rows > 0) {
             <div class="post-title">' . htmlspecialchars($row['title']) . '</div>
             <div class="post-desc">' . htmlspecialchars($row['description']) . '</div>
             <div class="post-category">Category: ' . htmlspecialchars($row['category_name']) . '</div>
-            <div class="post-tags">Tags: ' . $row['tag_names'] . '</div>
+            <div class="post-tags">Tags: ' . htmlspecialchars($row['tag_names']) . '</div>
             <div class="post-date">' . htmlspecialchars($row['date']) . '</div>
             <div class="post-comments"><a href="comments.php?post_id=' . $row['id'] . '">View Comments</a></div>
         </div>';
